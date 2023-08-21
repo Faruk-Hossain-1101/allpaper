364842a7e9ff25b99c1f51e3552dfa4be8f35dc5
Faruk Hossain <hossainf114@gmail.com>
2023-08-21 21:55:50 +0530

    First Commit

---
+++ accounts/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse, response
+
+import logging
+logger = logging.getLogger(__name__)
+logger = logging.getLogger('django')
+logger_api = logging.getLogger('api')
+
+def index(request):
+        
+    return HttpResponse("Hello, world. You're at the polls index.")
